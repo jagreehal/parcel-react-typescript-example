83216163080bac13c5dde7677fe904a1278ac8b4
jagreehal <jag.reehal@gmail.com>
2017-12-29 09:33:22 +0000

    updated deps

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,8 @@ import * as ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
 
-// ReactDOM.render(
-//   <App name="Jag"/>,
-//   document.getElementById('root')
-// );
 
 ReactDOM.render(
   <App name="Jag"/>,
   document.getElementById("root") as HTMLElement
 );
-
-// const hello = () => {
-//   return "Hello World!";
-// };
-// // main
-// const main = document.getElementById("root");
-// if (main) {
-//   main.textContent = hello();
-// }
